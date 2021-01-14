589f97bc2fb296c2bb7c14e68a35da0bc71617ab
Sarah <sarahchosen@hotmail.co.uk>
2021-01-14 10:10:46 +0000

    set up files to create bookmarks

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,22 +1,16 @@
 feature 'pg'
 
 feature 'viewing bookmarks' do
-  scenario 'A user can see bookmarks' do
-    
-
-
-  end
   scenario 'visiting the index page' do
     visit('/')
     expect(page).to have_content "Bookmark Manager"
   end
 
   scenario 'user clicks view button to view bookmarks' do
-    connection = PG.connect(dbname: 'bookmark_manager_test')
-
-    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://www.makersacademy.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://www.twitter.com');")
-    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
+  
+    Bookmark.create(url: 'http://www.makersacademy.com')
+    Bookmark.create(url: 'http://www.twitter.com')
+    Bookmark.create(url: 'http://www.google.com')
 
 
     visit('/bookmarks')
